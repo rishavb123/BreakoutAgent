9a107cac79aa3795710a4d78253f8c68fc3f381e
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2019-07-28 23:30:23 -0400

    basic imports and gitignore

---
+++ main.py
@@ -0,0 +1,11 @@
+# import copy
+# import gym
+# import os
+# import sys
+# import random
+# import numpy as np
+# import tensorflow as tf
+# import matplotlib.pyplot as plt
+# from gym import wrappers
+# from datetime import datetime
+from scipy.misc import imresize 
